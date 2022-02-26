77ed616ccf8cf2fa4c0166271a59b9bc07a557bc
XimeCruz <xmenacruzt@gmail.com>
2022-02-26 00:00:38 -0400

    Logging

--- src/main/java/com/ventas/Ventas/VentasApplication.java
+++ src/main/java/com/ventas/Ventas/VentasApplication.java
@@ -1,12 +1,21 @@
 package com.ventas.Ventas;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VentasApplication {
 
+//	private static final Logger logger= LoggerFactory.getLogger(VentasApplication.class);
+
 	public static void main(String[] args) {
+//		logger.trace("Este es un mensaje de rastreo");
+//		logger.debug("Este es un mensaje de depuracion");
+//		logger.info("Este es un mensaje de informacion");
+//		logger.warn("Este es un mensaje de advertencia");
+//		logger.error("Este es un mensaje de error");
 		SpringApplication.run(VentasApplication.class, args);
 	}
 
